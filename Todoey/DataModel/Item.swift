54162cc1cc897e81d79da7419fa342c5a6f9ba3d
Gabor Monyok <gabor.monyok@ge.com>
2018-01-06 16:26:04 +0100

    Fix reusableCell bug, refactor

---
+++ Todoey/DataModel/Item.swift
@@ -0,0 +1,17 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Monyok Gabor on 2018. 01. 06..
+//  Copyright © 2018. Monyok Gabor. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
+
+
+
